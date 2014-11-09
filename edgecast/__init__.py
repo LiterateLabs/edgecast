a653121218b31972894e175771528271f986592c
Eric Powers <eric@readz.com>
2014-11-08 17:58:08 -0800

    Add origin/cname methods.

--- edgecast/__init__.py
+++ edgecast/__init__.py
@@ -18,6 +18,12 @@ MEDIA_TYPE_APPLICATION_DELIVERY_NETWORK = 14
 """Application Delivery Network media type.
 """
 
+_MEDIA_TYPE_TO_URI_PATH = {
+    MEDIA_TYPE_FLASH_MEDIA_STREAMING: 'flash',
+    MEDIA_TYPE_HTTP_LARGE_OBJECT: 'httplarge',
+    MEDIA_TYPE_HTTP_SMALL_OBJECT: 'httpsmall',
+    MEDIA_TYPE_APPLICATION_DELIVERY_NETWORK: 'adn',
+}
 
 class EdgeCastError(Exception):
     """EdgeCast communications error.
@@ -47,17 +53,17 @@ class Client(object):
     def _request(self,
                  endpoint_method,
                  method,
-                 data):
+                 data = None):
         """Perform a request.
 
-        :param endpoint_method: Endpoint method such as ``purge``.
+        :param endpoint_method: Endpoint method such as ``edge/purge``.
         :param method: Request method.
         :param data: JSON request data.
         """
 
         response = self._session.request(
             method,
-            'https://api.edgecast.com/v2/mcc/customers/%s/edge/%s' % (
+            'https://api.edgecast.com/v2/mcc/customers/%s/%s' % (
                 self.account_number,
                 endpoint_method,
             ),
@@ -66,7 +72,7 @@ class Client(object):
                 'Content-Type': 'application/json',
                 'Authorization': 'TOK:%s' % (self.token),
             },
-            data=json.dumps(data)
+            data=json.dumps(data) if data else None
         )
 
         try:
@@ -77,6 +83,8 @@ class Client(object):
                     response.text
                 ))
 
+        return response
+
     def purge(self, media_type, *patterns):
         """Purge one or more resources from the CDN.
 
@@ -85,7 +93,7 @@ class Client(object):
         """
 
         for pattern in patterns:
-            self._request('purge',
+            self._request('edge/purge',
                           'PUT',
                           {'MediaPath': pattern, 'MediaType': media_type})
 
@@ -97,9 +105,55 @@ class Client(object):
         """
 
         for url in urls:
-            self._request('load',
+            self._request('edge/load',
                           'PUT',
                           {'MediaPath': url, 'MediaType': media_type})
+
+    def get_all_customer_origins(self, media_type):
+        """This method retrieves all customer origin configurations.
+
+        :param media_type: Media type.
+        """
+
+        s_media_type = _MEDIA_TYPE_TO_URI_PATH.get( media_type )
+        response = self._request('origins/%s' % s_media_type, 'GET')
+
+        return response.json()
+
+    def add_customer_origin(self, media_type, data):
+        """This method adds a customer origin.
+
+        :param media_type: Media type.
+        :param data: JSON request data.
+        """
+
+        s_media_type = _MEDIA_TYPE_TO_URI_PATH.get( media_type )
+        response = self._request('origins/%s' % s_media_type, 'POST', data)
+
+        return response.json()
+
+    def get_all_edge_cnames(self, media_type):
+        """This method allows the retrieval of all edge CNAMEs
+
+        :param media_type: Media type.
+        """
+
+        s_media_type = _MEDIA_TYPE_TO_URI_PATH.get( media_type )
+        response = self._request('cnames/%s' % s_media_type, 'GET')
+
+        return response.json()
+
+    def add_edge_cname(self, media_type, data):
+        """This method creates an edge CNAME.
+
+        :param media_type: Media type.
+        :param data: JSON request data.
+        """
+
+        data['MediaTypeId'] = media_type
+        response = self._request('cnames', 'POST', data)
+
+        return response.json()
 
 
 __all__ = (
